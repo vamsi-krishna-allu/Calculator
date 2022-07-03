0d76652b80cf05e16270d7910ff98975bf101dff
A00259393-Vamsi <A00259393@mycambrian.ca>
2022-07-03 08:29:31 -0400

    JAVA-1001-Calculator: UI layout for calculator app

--- app/src/main/java/com/lab/calculator/MainActivity.java
+++ app/src/main/java/com/lab/calculator/MainActivity.java
@@ -3,6 +3,7 @@ package com.lab.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // setting the full screen flag to hide status bar
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 }
